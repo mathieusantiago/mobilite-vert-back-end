e51ac13199a4f304c0b68f664b052f233e36cc2c
mathieusantiago <mathieusantiago21@gmail.com>
2022-10-19 22:15:59 +0200

    build

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -64,7 +64,6 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.header("Access-Control-Allow-Origin", "*");
     res.cookie("jwt", token, { path: '/', domain:'netlify.app', httpOnly: true, maxAge });
     res.status(200).json({ user: user._id });
   } catch (err) {
