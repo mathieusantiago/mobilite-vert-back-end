36229180a570ade989769ecd899f05030ef12d00
mathieusantiago <mathieusantiago21@gmail.com>
2022-10-20 17:08:08 +0200

    debloy

--- index.js
+++ index.js
@@ -23,8 +23,6 @@ const gAnalytics = require("./routes/gAnalytics.routes.js");
 const corsOptions = {
   origin:['*', process.env.CLIENT_URL, process.env.DOCS_URL, process.env.BACKOFFICE_URL],
   credentials: true,
-  allowedHeaders: ["sessionId", "Content-Type"],
-  exposedHeaders: ["sessionId"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
 };
