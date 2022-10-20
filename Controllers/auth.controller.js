aaba409e48976ca03031a02ad139b7bcc1cc5f45
mathieusantiago <mathieusantiago21@gmail.com>
2022-10-20 10:36:22 +0200

    bluild

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -64,7 +64,7 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwt", token, { domain:'onrender.com', httpOnly: false, maxAge });
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
     res.status(200).json({ user: user._id });
   } catch (err) {
     const errors = signInErrors(err);
